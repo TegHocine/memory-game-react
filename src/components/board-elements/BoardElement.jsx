a5df759951699a54d9f84a827f2752ddcd6f4da9
TegHocine <arcou98@hotmail.com>
2022-06-13 10:05:30 +0100

    tailwind and ui skeleton

---
+++ src/components/board-elements/BoardElement.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const BoardElement = () => {
+  return <div>BoardElement</div>
+}
+
+export default BoardElement
---
+++ src/components/board/Board.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import BoardElement from '../board-elements/BoardElement'
+
+const Board = () => {
+  return (
+    <div>
+      <BoardElement />
+      <BoardElement />
+      <BoardElement />
+      <BoardElement />
+      <BoardElement />
+      <BoardElement />
+      <BoardElement />
+      <BoardElement />
+      <BoardElement />
+      <BoardElement />
+      <BoardElement />
+      <BoardElement />
+    </div>
+  )
+}
+
+export default Board
